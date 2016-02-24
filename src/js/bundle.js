c6c3cf75e2580ef6a1cac7d17e35fc831aed2b3c
Yurii Buts <yura@Yuriis-MacBook-Pro.local>
2016-02-24 02:07:37 +0200

    clear unused scripts from bundle.js and add scrolling navigation

--- src/js/bundle.js
+++ src/js/bundle.js
@@ -1,13 +1,7 @@
 //= scripts/jquery.min.js
 //= scripts/bootstrap.min.js
 //= scripts/classie.js
-//= scripts/jquery-validate.js
 //= scripts/jquery.easing.min.js
-//= scripts/jquery.fancybox.js
-//= scripts/jquery.validationEngine-ru.js
-//= scripts/owl.carousel.js
 //= scripts/scrolling-nav.js
-//= scripts/inputmask.js
-//= scripts/jquery.inputmask.js
-//= scripts/equel.js
+//= scripts/owl.carousel.js
 //= scripts/scripts.js
--- src/js/scripts/scripts.js
+++ src/js/scripts/scripts.js
@@ -1,23 +1,5 @@
 $(document).ready(function() {
 
-    /* Input Mask */
-    $('#tel').inputmask("(999) 999-99-99");
-
-    /* Fancybox */
-    $(".team-image-overlay").fancybox({
-        prevEffect	: 'none',
-        nextEffect	: 'none',
-        padding :7,
-        helpers	: {
-            title	: {
-                type: 'outside'
-            },
-            overlay: {
-                locked: false
-            }
-        }
-    });
-
     /* Owl Testimonials */
     $("#testimonials-carousel").owlCarousel({
         items : 1,
@@ -33,65 +15,6 @@ $(document).ready(function() {
         paginationNumbers: false
     });
 
-    /* Owl Lastest Works */
-    $("#latest-works-corousel").owlCarousel({
-        items : 3,
-        itemsCustom : false,
-        itemsDesktop : [1199,3],
-        itemsDesktopSmall : [980,2],
-        itemsTablet: [768,2],
-        itemsTabletSmall: [600,1],
-        itemsMobile : [479,1],
-        autoPlay:true,
-        pagination:true,
-        stopOnHover : false,
-        paginationNumbers: false,
-        navigation: true,
-        navigationText:	["<i class=\'fa fa-arrow-left\'></i>","<i class=\'fa fa-arrow-right\'></i>"]
-
-
-    });
-
-    /* Navbar fixed class */
-    function init() {
-        "use strict";
-        window.addEventListener('scroll', function(e){
-            var distanceY = window.pageYOffset || document.documentElement.scrollTop,
-                shrinkOn = 250,
-                header = document.querySelector(".navbar");
-            if (distanceY > shrinkOn) {
-                classie.add(header,"fixed");
-            } else {
-                if (classie.has(header,"fixed")) {
-                    classie.remove(header,"fixed");
-                }
-            }
-        });
-    };
-    window.onload = init();
-
-    /* Form validation */
-    jQuery("#contact-form").validationEngine({
-        promptPosition : "bottomLeft",
-        scroll: scroll,
-        autoHidePrompt: true,
-        autoHideDelay: 3000,
-        scrollOffset: 100,
-        binded: false
-    });
-
-
-
-});
-
-    /* Height Equelazing */
-$(function() {
-    $('.latest-work-text').matchHeight();
-    $('.media').matchHeight({
-        byRow: false,
-        property: 'height'
-    });
-    $('.team-text').matchHeight();
 });
 
 
