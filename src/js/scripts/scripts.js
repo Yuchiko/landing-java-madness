31e8f07d4565d1cf0197a6bf9e401a3f8fd9ffee
Yurii Buts <yura@Yuriis-MacBook-Pro.local>
2016-02-25 05:37:00 +0200

    add animation for some elements on page scroll

--- src/js/scripts/scripts.js
+++ src/js/scripts/scripts.js
@@ -1,20 +1,17 @@
 $(document).ready(function() {
-
-    /* Owl Testimonials */
-    $("#testimonials-carousel").owlCarousel({
-        items : 1,
-        itemsCustom : false,
-        itemsDesktop : [1199,1],
-        itemsDesktopSmall : [980,1],
-        itemsTablet: [768,1],
-        itemsTabletSmall: false,
-        itemsMobile : [479,1],
-        autoPlay:true,
-        pagination:true,
-        stopOnHover : false,
-        paginationNumbers: false
-    });
-
+    var wow = new WOW(
+        {
+            boxClass:     'wow',      // animated element css class (default is wow)
+            animateClass: 'animated', // animation css class (default is animated)
+            offset:       0,          // distance to the element when triggering the animation (default is 0)
+            mobile:       true,       // trigger animations on mobile devices (default is true)
+            live:         true,       // act on asynchronously loaded content (default is true)
+            callback:     function(box) {
+            },
+            scrollContainer: null // optional scroll container selector, otherwise use window
+        }
+    );
+    wow.init();
 });
 
 
